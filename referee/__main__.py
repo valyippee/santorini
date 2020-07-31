d9a0ba399944b8e5ab224c7dd17c2ef438481a99
valyippee <valerieyipfh@gmail.com>
2020-07-31 15:16:59 +0800

    completed __main__.py

---
+++ referee/__main__.py
@@ -0,0 +1,17 @@
+from santorini.referee.game import play
+from santorini.referee.player import PlayerWrapper
+from santorini.referee.options import get_options
+
+
+def main():
+
+    options = get_options
+
+    p1 = PlayerWrapper("red", options.player1_loc)
+    p2 = PlayerWrapper("yellow", options.player2_loc)
+    result = play([p1, p2])
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
