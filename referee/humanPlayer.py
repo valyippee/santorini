29665f1f6b121607ca6086df481e06dee5d776b5
valyippee <valerieyipfh@gmail.com>
2020-07-31 18:29:37 +0800

    added human player

---
+++ referee/humanPlayer.py
@@ -0,0 +1,14 @@
+from board import Board
+
+
+class Player:
+    def __init__(self, colour):
+        self.colour = colour
+        self.board = Board()
+
+    def update(self, player_actions):
+        self.board.move_and_build(player_actions)
+
+    def action(self):
+        chosen_move = input("Please enter move: ")
+        return chosen_move
