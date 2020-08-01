879ddd3036e05c23b9874d680ed005067be7cca2
valyippee <valerieyipfh@gmail.com>
2020-08-01 11:50:12 +0800

    added parser

--- referee/humanPlayer.py
+++ referee/humanPlayer.py
@@ -11,11 +11,17 @@ class Player:
 
     def action(self):
         chosen_move = input("Please enter move: ")
-        return list(eval(chosen_move))
+        move_list = chosen_move.split(" ")
+        for i in range(len(move_list)):
+            move_list[i] = int(move_list[i])
+        return (move_list[0], move_list[1]), (move_list[2], move_list[3]), (move_list[4], move_list[5])
 
     def choose_starting_loc(self):
         chosen_move = input("Please enter starting locations: ")
-        return list(eval(chosen_move))
+        move_list = chosen_move.split(" ")
+        for i in range(len(move_list)):
+            move_list[i] = int(move_list[i])
+        return (move_list[0], move_list[1]), (move_list[2], move_list[3])
 
     def update_starting_loc(self, player_colour, starting_loc):
         for location in starting_loc:
